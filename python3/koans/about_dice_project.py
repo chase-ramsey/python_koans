836ed8a1a41478482e7bd6ce0a068ae96991320c
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-22 14:14:26 -0500

    Completed three more koans
    about_dice_project, _monkey_patching, _with_statements

--- python3/koans/about_dice_project.py
+++ python3/koans/about_dice_project.py
@@ -7,7 +7,7 @@ import random
 
 class DiceSet:
     def __init__(self):
-        self._values = None
+        self._values = list()
 
     @property
     def values(self):
@@ -16,7 +16,12 @@ class DiceSet:
     def roll(self, n):
         # Needs implementing!
         # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = []
+        i = 0
+        while i < n:
+            i += 1
+            rand_num = random.randint(1, 6)
+            self.values.append(rand_num)
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
